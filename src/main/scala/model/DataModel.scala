c034c09d2f76697f69aac9c5706984915d5c7186
Maxime WANG <Maxime.WANG@3ds.com>
2024-01-04 20:32:45 +0100

    Init db script & init model repo

---
+++ src/main/scala/model/DataModel.scala
@@ -0,0 +1,21 @@
+package model
+
+object DataModel {
+
+  opaque type Latitude = Double
+  opaque type Longitude = Double
+  opaque type CityName = String
+  opaque type Date = String
+
+  case class Coordinates(latitude: Latitude, longitude: Longitude)
+
+  case class City(name: CityName, coordinates: Coordinates)
+
+  case class CurrentWeather(coordinates: Coordinates,
+                            temperature: Double,
+                            weatherDescription: String,
+                            date: Date)
+
+  enum WeatherStatus:
+    case Sun, Rain, Snow
+}
