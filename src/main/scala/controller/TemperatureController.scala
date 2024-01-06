abbd6b83e137880a96806bbaed24d28417b4c93d
nom2 <nom2@email.com>
2024-01-06 02:32:04 +0100

    début implémentation route get temperature

--- src/main/scala/controller/TemperatureController.scala
+++ src/main/scala/controller/TemperatureController.scala
@@ -1,5 +1,21 @@
 package controller
 
-class TemperatureController {
+import zio._
+import zio.http._
+import zio.stream._
+import service.TemperatureService
+import model.DataModel._
+import zio.Duration
 
+
+trait TemperatureController {
+  def getTemperature(city: CityName): ZIO[Any, HttpError, Double]
+}
+
+object TemperatureController {
+  def getTemperature(city: CityName): ZIO[Has[TemperatureService] with Has[Client], HttpError, Double] =
+    TemperatureService.getTemperature(city).runHead.flatMap {
+      case Some(temperature) => ZIO.succeed(temperature)
+      case None => ZIO.fail(HttpError.NotFound(s"Temperature not found for city: $city"))
+    }
 }
--- src/main/scala/service/TemperatureService.scala
+++ src/main/scala/service/TemperatureService.scala
@@ -1,5 +1,50 @@
 package service
 
-class TemperatureService {
+import zio._
+import zio.http._
+import zio.stream._
+import model.DataModel._
+import zio.Duration
 
+
+trait TemperatureService {
+  def getTemperature(city: CityName): ZStream[Client, Throwable, Double]
+}
+
+object TemperatureService {
+  def live: ZLayer[Has[Client], Nothing, Has[TemperatureService]] =
+    ZLayer.fromFunction { client =>
+      new TemperatureService {
+        val baseUrl = URL
+          .decode("https://api.openweathermap.org/data/2.5/weather")
+          .toOption
+          .get
+
+        def getTemperature(city: CityName): ZStream[Client, Throwable, Double] = {
+          ZStream
+            .fromEffect(CoordinateService.getCityToCoord(city))
+            .flatMap(coord => fetchData(coord.latitude.toString, coord.longitude.toString))
+            .mapM(res => res.body.asString.map(body => decodeJson[CurrentWeather](body)))
+            .collect { case Right(weather) => weather.temperature }
+        }
+
+        private def fetchData(latitude: String, longitude: String): ZStream[Client, Throwable, Response] = {
+          val url = baseUrl
+            .addQueryParams(
+              QueryParams(
+                Map(
+                  "lat" -> Chunk(latitude),
+                  "lon" -> Chunk(longitude),
+                  "appid" -> Chunk("d1973fd69722dbf4497b5701fb60f939")
+                )
+              )
+            )
+
+          ZStream.fromEffect(client.get(url))
+        }
+      }
+    }
+
+  def getTemperature(city: CityName): ZStream[Has[TemperatureService] with Has[Client], Throwable, Double] =
+    ZStream.accessStream(_.get[Has[TemperatureService]].get.getTemperature(city))
 }
