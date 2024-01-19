094637064cd50109069dae00f11dbc177683ef43
nom2 <nom2@email.com>
2024-01-19 17:31:51 +0100

    end tests temperature service

--- src/test/scala/TemperatureServiceSpec.scala
+++ src/test/scala/TemperatureServiceSpec.scala
@@ -5,7 +5,6 @@ import model.{DataModel, DataModelDao, DatabaseManager}
 
 object TemperatureServiceSpec extends ZIOSpecDefault {
 
-
   override def spec = suite("TestingApplicationsExamplesSpec")(
     suite("TemperatureService")(
       test("getTemperatureOfCity should return the correct response when temperature data is available") {
@@ -33,10 +32,49 @@ object TemperatureServiceSpec extends ZIOSpecDefault {
         val result = service.getTemperatureOfCity(cityName)
 
         assert(result)(equalTo("Failed to retrieve cities data."))
+      },
+
+      test("getRecommandationByCity should return the correct recommendation when all data is available") {
+        val cityName = "Paris"
+        val service = new TemperatureServiceMock(DataModel.mockCityData(Some(cityName)))
+
+        val result = service.getRecommandationByCity(cityName)
+
+        assert(result)(
+          equalTo("The recommendation for the city of Paris is: Enjoy the moderate weather!")
+        )
+      },
+
+      test("getRecommandationByCity should return an error message when city data is not available") {
+        val cityName = "NonExistentCity"
+        val service = new TemperatureServiceMock(DataModel.mockCityData(None))
+
+        val result = service.getRecommandationByCity(cityName)
+
+        assert(result)(equalTo("Failed to retrieve cities data."))
+      },
+
+      test("getRecommandationByCity should return an error message when temperature data is not available") {
+        val cityName = "Paris"
+        val service = new TemperatureServiceMock(DataModel.mockCityData(None))
+
+        // Mocking temperature data to be None
+        val result = service.getRecommandationByCity(cityName)
+
+        assert(result)(equalTo("Failed to retrieve cities data."))
+      },
+
+      test("getRecommandationByCity should return an error message when recommendation data is not available") {
+        val cityName = "Paris"
+        val service = new TemperatureServiceMock(DataModel.mockCityData(None))
+
+        // Mocking recommendation data to be None
+        val result = service.getRecommandationByCity(cityName)
+
+        assert(result)(equalTo("Failed to retrieve cities data."))
       }
     )
   )
-
 
   case class TemperatureServiceMock(cityData: Option[model.DataModel.City]) extends TemperatureService {
     override def getTemperatureOfCity(cityName: String): CharSequence = {
@@ -45,17 +83,35 @@ object TemperatureServiceSpec extends ZIOSpecDefault {
         case Some(city) =>
           val temperatureOption = DataModel.mockTemperatureData(city.coordinates)
           temperatureOption.map { temperatureOfCity =>
-            s"The temperature in $cityName is ${temperatureOfCity.asInstanceOf} degrees Celsius."
+            s"The temperature in $cityName is $temperatureOfCity degrees Celsius."
           }.getOrElse("Failed to retrieve temperature data for the city.")
         case None => "Failed to retrieve cities data."
       }
       result
     }
-    override def getRecommandationByCity(cityName: String): CharSequence = ???
+
+    override def getRecommandationByCity(cityName: String): CharSequence = {
+      val cityOption = cityData.orElse(DataModel.mockCityData(None))
+      val result: CharSequence = cityOption match {
+        case Some(city) =>
+          val temperatureOption = DataModel.mockTemperatureData(city.coordinates)
+          temperatureOption match {
+            case Some(temperatureOfCity) =>
+              val recommendationOption = DataModel.mockRecommendationData(temperatureOfCity)
+              recommendationOption.map { recommendation =>
+                s"The recommendation for the city of $cityName is: ${recommendation.recommendation}"
+              }.getOrElse("Failed to retrieve recommendation data for the city.")
+            case None =>
+              "Failed to retrieve temperature data for the city."
+          }
+        case None => "Failed to retrieve cities data."
+      }
+      result
+    }
   }
 
   object DataModel {
-    import model.DataModel.{City, Coordinates, Temperature}
+    import model.DataModel.{City, Coordinates, Temperature, TemperatureRecommendation}
 
     def mockCityData(cityName: Option[String]): Option[City] =
       val latitude = 48.8566
@@ -65,6 +121,12 @@ object TemperatureServiceSpec extends ZIOSpecDefault {
     def mockTemperatureData(coordinates: Coordinates): Option[Double] =
       if (coordinates.latitude.asInstanceOf == 48.8566 && coordinates.longitude.asInstanceOf == 2.3522)
         Some(22.5)
+      else
+        None
+
+    def mockRecommendationData(temperature: Double): Option[TemperatureRecommendation] =
+      if (temperature >= 20)
+        Some(TemperatureRecommendation("Moderate", None, Some(30.0), "Enjoy the moderate weather!"))
       else
         None
   }
